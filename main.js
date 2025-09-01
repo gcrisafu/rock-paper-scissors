5c6b12044570fc865344597bf26c477a5ba5865c
gcrisafu <25827246+gcrisafu@users.noreply.github.com>
2025-08-31 21:22:49 -0400

    if else statement logic for round function. Compares convertedUserChoice and CompChoice then  prints both the compChoice and the result of the game

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-console.log("Hello World")
+console.log("Rock, Paper, Scissors")
 // gets computer's choice
 function computerChoice(){
     let computer = Math.random();
     if (computer < 1/3)
-        {return "Rock"}
+        {return "rock"}
     else if (computer > 1/3 && computer < 2/3 )
-        {return "Paper"}
+        {return "paper"}
     else (computer > 2/3)
-        {return "Scissors"}
+        {return "scissors"}
     }
 
 let compChoice = computerChoice();
+
+// get user input and converts to lower case
 
 let userChoice = prompt();
 
@@ -19,7 +21,53 @@ function convertUserInput (userChoice){
 }
 
 let convertedUserChoice = convertUserInput(userChoice)
-console.log(convertedUserChoice)
+
+// round function, compares convertedUserChoice and compChoice
+
+function round (convertedUserChoice,compChoice){
+    // User throws rock
+    if ((convertedUserChoice === "rock") && (compChoice == "scissors")){
+        console.log(compChoice)
+        console.log("You win! Rock beats scissors")
+        return true
+    } else if ((convertedUserChoice === "rock") && (compChoice === "paper")){
+        console.log(compChoice)
+        console.log("You lose... Paper beats rock")
+        return false
+    } else if ((convertedUserChoice === "rock") && (compChoice === "rock")){
+        console.log(compChoice)
+        console.log("Draw")
+        return null
+    // User throw paper
+    } else if ((convertedUserChoice === "paper") && (compChoice === "rock")){
+        console.log(compChoice)
+        console.log("You win! Paper beats rock")
+        return true
+    } else if ((convertedUserChoice === "paper") && (compChoice === "scissors")){
+        console.log(compChoice)
+        console.log("You lose... Scissors beats paper")
+        return false 
+     } else if ((convertedUserChoice === "paper") && (compChoice === "paper")){
+        console.log(compChoice)
+        console.log("Draw")
+        return null
+    // User throws scissors 
+    } else if ((convertedUserChoice === "scissors") && (compChoice === "paper")){
+        console.log(compChoice)
+        console.log("You win! Scissors beats paper")
+        return true
+    } else if ((convertedUserChoice === "scissors") && (compChoice === "rock")){
+        console.log(compChoice)
+        console.log("You lose... Rock beats scissors")
+        return false 
+     } else if ((convertedUserChoice === "scissors") && (compChoice === "scissors")){
+        console.log(compChoice)
+        console.log("draw")
+        return null 
+     } 
+}
+
+let game = round(convertedUserChoice,compChoice)
 
 
 
