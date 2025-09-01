e8b7538aaf8a9674baa15c959764657af7f7dec1
gcrisafu <25827246+gcrisafu@users.noreply.github.com>
2025-08-31 20:23:50 -0400

    Computer Choice added to main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+console.log("Hello World")
+
+function computerChoice(){
+    let computer = Math.random();
+    if (computer < 1/3)
+        {return "Rock"}
+    else if (computer > 1/3 && computer < 2/3 )
+        {return "Paper"}
+    else (computer > 2/3)
+        {return "Scissors"}
+    }
+
+let compChoice = computerChoice()
+console.log(compChoice)
+
+
+
+
